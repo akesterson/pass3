33c5735c80eb5671f9a187576044a8474ed443bf
Andrew Kesterson <andrew@aklabs.net>
2015-09-13 15:01:25 -0700

    Engine add/store interface works, unit tested, needs encryption and the S3 backend

---
+++ pass3/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+from pass3.engine import Engine
+from pass3.record import Record
+from pass3.storage import SimpleStorage
--- tests/test_store_retrieve.py
+++ tests/test_store_retrieve.py
@@ -3,93 +3,104 @@ import pass3
 
 def test_store_retrieve():
     engine = pass3.Engine(
-        password='testing',
-        access_key='testing',
-        storage_key='testing')
-    stored_obj = engine.store(
+        password='testing')
+    entry = pass3.Record(
         scheme='http',
-        uri='localhost.localdomain/somesite',
+        host='localhost.localdomain',
+        path='/somesite',
         user='test',
-        password='test'
+        password='test',
+        title='Some Web Page : Welcome'
         )
-    records = engine.search(
-        scheme='http',
-        uri='localhost.localdomain/somesite'
-        )
+    engine.store(entry)
+    records = [x for x in engine.search(scheme='http', host='localhost.localdomain', path='/somesite')]
     assert( len(records) == 1 )
     assert( records[0].scheme == 'http' )
-    assert( records[0].uri == 'localhost.localdomain/somesite' )
+    assert( records[0].host == 'localhost.localdomain' )
+    assert( records[0].path == '/somesite' )
     assert( records[0].user == 'test' )
     assert( records[0].password == 'test' )
+    assert( records[0].title == 'Some Web Page : Welcome' )
 
 def test_store_retrieve_multiple():
     engine = pass3.Engine(
-        password='testing',
-        access_key='testing',
-        storage_key='testing')
-    stored_obj = engine.store(
-        scheme='http',
-        uri='localhost.localdomain/somesite',
-        user='test',
-        password='test'
+        password='testing'
         )
-    stored_obj = engine.store(
-        scheme='http',
-        uri='localhost.localdomain/some_other_site',
-        user='test',
-        password='test'
+    engine.store(
+        pass3.Record(
+            scheme='http',
+            host='localhost.localdomain',
+            path='/somesite',
+            user='test',
+            password='test',
+            title='test'
         )
-    records = engine.search(
-        scheme='http',
-        uri='localhost.localdomain'
+    )
+    engine.store(
+        pass3.Record(
+            scheme='http',
+            host='localhost.localdomain',
+            path='/some_other_site',
+            user='test',
+            password='test',
+            title='test 2'
         )
+    )
+    records = [x for x in engine.search(scheme='http', host='localhost.localdomain')]
     assert( len(records) == 2 )
     assert( records[0].scheme == 'http' )
-    assert( records[0].uri == 'localhost.localdomain/somesite' )
+    assert( records[0].host == 'localhost.localdomain' )
+    assert( records[0].path == '/somesite' )
     assert( records[0].user == 'test' )
     assert( records[0].password == 'test' )
     
     assert( records[1].scheme == 'http' )
-    assert( records[1].uri == 'localhost.localdomain/some_other_site' )
+    assert( records[1].host == 'localhost.localdomain' )
+    assert( records[1].path == '/some_other_site' )
     assert( records[1].user == 'test' )
     assert( records[1].password == 'test' )
 
 def test_store_retrieve_alternate():
     engine = pass3.Engine(
-        password='testing',
-        access_key='testing',
-        storage_key='testing')
-    stored_obj = engine.store(
+        password='testing'
+        )
+    record = pass3.Record(
         scheme='http',
-        uri='localhost.localdomain/somesite',
+        host='localhost.localdomain',
+        path='/somesite',
         user='test',
-        password='test'
+        password='test',
+        title='test'
         )
-    alternate_obj = engine.add_alternate(
-        target=stored_obj,
-        scheme='http',
-        uri='localhost.localdomain/some_other_site'
+    record.add_alternate(
+        pass3.Record(
+            scheme='http',
+            host='localhost.localdomain',
+            path='/some_other_site'
         )
-    records = engine.search(
-        scheme='http',
-        uri='localhost.localdomain/somesite'
-        )
+    )
+    engine.store(record)
+    records = [x for x in engine.search(scheme='http', host='localhost.localdomain', path='/somesite')]
     assert( len(records) == 1 )
     assert( records[0].scheme == 'http' )
-    assert( records[0].uri == 'localhost.localdomain/somesite' )
+    assert( records[0].host == 'localhost.localdomain' )
+    assert( records[0].path == '/somesite' )
     assert( records[0].user == 'test' )
     assert( records[0].password == 'test' )
-
-    records = engine.search(
-        scheme='http',
-        uri='localhost.localdomain/some_other_site'
-        )
+    assert( len(records[0].alternates) == 1 )
+    assert( records[0].alternates[0].scheme == 'http' )
+    assert( records[0].alternates[0].host == 'localhost.localdomain' )
+    assert( records[0].alternates[0].path == '/some_other_site' )
+    
+    records = [x for x in engine.search(scheme='http', host='localhost.localdomain', path='some_other_site')]
     assert( len(records) == 1 )
     assert( records[0].scheme == 'http' )
-    assert( records[0].uri == 'localhost.localdomain/somesite' )
+    assert( records[0].host == 'localhost.localdomain' )
+    assert( records[0].path == '/somesite' )
     assert( records[0].user == 'test' )
     assert( records[0].password == 'test' )
 
     assert( len(records[0].alternates) == 1 )
     assert( records[0].alternates[0].scheme == 'http' )
-    assert( records[0].alternates[0].uri == 'localhost.localdomain/some_other_site' )
+    assert( records[0].alternates[0].host == 'localhost.localdomain' )
+    assert( records[0].alternates[0].path == '/some_other_site' )
